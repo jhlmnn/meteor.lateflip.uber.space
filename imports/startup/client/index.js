3f195a2fe15530c7faa36327bbafa1e6e21f7a13
Peter <mail@joerg-heilmann.de>
2019-12-09 11:01:00 +0100

    Store the session in localStorage

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,10 +4,16 @@ import { Session } from 'meteor/session';
 import './routes.js';
 
 Meteor.startup(() => {
-  Meteor.call('client_sessions.start_session', (error,result) => {
-    if (result){
-      // Set the client_id for this session
-      Session.set("client_id",result);
-    }
-  })
+  //
+  if (localStorage["client_id"]==undefined) {
+    Meteor.call('client_sessions.start_session', (error,result) => {
+      if (result){
+        // Set the client_id for this session
+        localStorage["client_id"] = result;
+        Session.set("client_id",result);
+      }
+    });
+  } else {
+    Session.set("client_id",localStorage["client_id"]);
+  }
 });
